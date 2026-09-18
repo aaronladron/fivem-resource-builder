72e1c529e388f3ba49aacbbcb430dce108e58974
aaronladron <aaron.ladron@epitech.eu>
2026-09-18 10:16:33 +0200

    ajout de la gestion des commandes CLI

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import fs from "node:fs";
+
+const args = process.argv;
+
+const command = args[2];
+const resourceName = args[3];
+
+if (!command) {
+    console.log("Commande non trouvée.");
+} else if (command === "create") {
+
+    if (!resourceName) {
+        console.log("Nom de la ressource non trouvé.");
+    } else {
+        console.log(`Création de la ressource : ${resourceName}`);
+        fs.mkdirSync(`./${resourceName}`);
+    }
+
+} else if (command === "list") {
+
+    console.log("Liste des ressources.");
+
+} else {
+
+    console.log(`Commande inconnue : ${command}`);
+}
